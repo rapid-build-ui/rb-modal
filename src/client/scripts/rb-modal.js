c87fc9c42dba87a76efc25539655ac92c6b1b26e
Judd Younce <jyounce@yahoo.com>
2018-08-30 22:12:44 -0500

    feat(rb-modal): init

---
+++ src/client/scripts/rb-modal.js
@@ -0,0 +1,23 @@
+/***********
+ * RB-MODAL
+ ***********/
+import { props, html, RbBase } from '../../rb-base/scripts/rb-base.js';
+import template from '../views/rb-modal.html';
+
+export class RbModal extends RbBase() {
+	/* Properties
+	 *************/
+	static get props() {
+		return {
+			kind: props.string
+		};
+	}
+
+	/* Template
+	 ***********/
+	render({ props }) { // :string
+		return html template;
+	}
+}
+
+customElements.define('rb-modal', RbModal);
